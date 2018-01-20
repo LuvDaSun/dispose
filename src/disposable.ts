715bacd1d7452149362331340c85abe34f67cb9e
Elmer Bulthuis <elmerbulthuis@gmail.com>
2018-01-20 20:47:59 +0100

    DisposableComposition

--- src/disposable.ts
+++ src/disposable.ts
@@ -9,3 +9,20 @@ export function isDisposable(
 ): maybeDisposable is Disposable {
     return typeof maybeDisposable.dispose === "function";
 }
+
+export class DisposableComposition implements Disposable {
+    private disposables = new Set<Disposable>();
+
+    public async dispose() {
+        const disposables = Array.from(this.disposables.values());
+        await Promise.all(disposables.map(disposable => disposable.dispose()));
+    }
+
+    protected registerDisposable(disposable: Disposable) {
+        this.disposables.add(disposable);
+    }
+    protected deregisterDisposable(disposable: Disposable) {
+        this.disposables.delete(disposable);
+    }
+
+}
