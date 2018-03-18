e1c5d51430910789807efb07a716d7ddbfd34f7f
Elmer Bulthuis <elmerbulthuis@gmail.com>
2018-03-18 13:34:02 +0100

    no arrays

--- src/using.spec.ts
+++ src/using.spec.ts
@@ -19,24 +19,6 @@ test("using", async t => {
         t.equal(c.disposed, true);
     }
 
-    {
-        const c = new Context();
-        await using([c], ([c]) => {
-            t.equal(c.disposed, false);
-        });
-        t.equal(c.disposed, true);
-    }
-
-    {
-        const c = new Context();
-        const c1 = new Context();
-        await using([c, c1], ([c, c1]) => {
-            t.equal(c.disposed, false);
-            t.equal(c1.disposed, false);
-        });
-        t.equal(c.disposed, true);
-        t.equal(c1.disposed, true);
-    }
 });
 
 test("using unusable", async t => {
@@ -44,29 +26,6 @@ test("using unusable", async t => {
         const u = {} as any;
         try {
             await using(u, u => null);
-            t.fail();
-        }
-        catch (err) {
-            t.pass();
-        }
-    }
-
-    {
-        const u = {} as any;
-        try {
-            await using([u], ([u]) => null);
-            t.fail();
-        }
-        catch (err) {
-            t.pass();
-        }
-    }
-
-    {
-        const c = new Context();
-        const u = {} as any;
-        try {
-            await using([u, c], ([u, c]) => null);
             t.fail();
         }
         catch (err) {
--- src/using.ts
+++ src/using.ts
@@ -2,106 +2,17 @@ import { Disposable, isDisposable } from "./disposable";
 
 export type Usable<TDisposable extends Disposable> = TDisposable | PromiseLike<TDisposable>;
 
-//#region tuples
-export async function using<TResult,
-    TDisposable1 extends Disposable,
-    TDisposable2 extends Disposable,
-    TDisposable3 extends Disposable,
-    TDisposable4 extends Disposable,
-    TDisposable5 extends Disposable>(
-    usables: [
-        Usable<TDisposable1>,
-        Usable<TDisposable2>,
-        Usable<TDisposable3>,
-        Usable<TDisposable4>,
-        Usable<TDisposable5>],
-    job: (disposables: [
-        TDisposable1,
-        TDisposable2,
-        TDisposable3,
-        TDisposable4,
-        TDisposable5]) => TResult,
-): Promise<TResult>;
-export async function using<TResult,
-    TDisposable1 extends Disposable,
-    TDisposable2 extends Disposable,
-    TDisposable3 extends Disposable,
-    TDisposable4 extends Disposable>(
-    usables: [
-        Usable<TDisposable1>,
-        Usable<TDisposable2>,
-        Usable<TDisposable3>,
-        Usable<TDisposable4>],
-    job: (disposables: [
-        TDisposable1,
-        TDisposable2,
-        TDisposable3,
-        TDisposable4]) => TResult,
-): Promise<TResult>;
-export async function using<TResult,
-    TDisposable1 extends Disposable,
-    TDisposable2 extends Disposable,
-    TDisposable3 extends Disposable>(
-    usables: [
-        Usable<TDisposable1>,
-        Usable<TDisposable2>,
-        Usable<TDisposable3>],
-    job: (disposables: [
-        TDisposable1,
-        TDisposable2,
-        TDisposable3]) => TResult,
-): Promise<TResult>;
-export async function using<TResult,
-    TDisposable1 extends Disposable,
-    TDisposable2 extends Disposable>(
-    usables: [
-        Usable<TDisposable1>,
-        Usable<TDisposable2>],
-    job: (disposables: [
-        TDisposable1,
-        TDisposable2]) => TResult,
-): Promise<TResult>;
-export async function using<TResult, TDisposable extends Disposable>(
-    usables: [Usable<TDisposable>],
-    job: (disposables: [TDisposable]) => TResult,
-): Promise<TResult>;
-//#endregion
-
-export async function using<TResult, TDisposable extends Disposable>(
-    usables: Array<Usable<TDisposable>>,
-    job: (disposables: TDisposable[]) => TResult,
-): Promise<TResult>;
 export async function using<TResult, TDisposable extends Disposable>(
     usable: Usable<TDisposable>,
     job: (disposable: TDisposable) => TResult,
-): Promise<TResult>;
-
-export async function using<TResult, TDisposable extends Disposable>(
-    usable: Usable<TDisposable> | Array<Usable<TDisposable>>,
-    job: (disposable: TDisposable | TDisposable[]) => TResult,
 ): Promise<TResult> {
-    if (Array.isArray(usable)) {
-        const disposable = await Promise.all(usable);
-        if (!disposable.every(isDisposable))
-            throw new Error(`Not all are disposable`);
-        try {
-            return await job(disposable);
-        }
-        finally {
-            await Promise.all(
-                disposable.map(disposable => disposable.dispose()),
-            );
-        }
+    const disposable = await usable;
+    if (!isDisposable(disposable))
+        throw new Error(`Not a disposable`);
+    try {
+        return await job(disposable);
     }
-    else {
-        const disposable = await usable;
-        if (!isDisposable(disposable))
-            throw new Error(`Not a disposable`);
-        try {
-            return await job(disposable);
-        }
-        finally {
-            await disposable.dispose();
-        }
+    finally {
+        await disposable.dispose();
     }
 }
